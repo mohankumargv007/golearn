f6d5243ed83aaab77efbdab7b40d296a7cb9ae47
venkata.mohan <venkatamohankumar.g@landmarkwebteam.com>
2021-11-24 17:36:45 +0530

    Updated code

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,14 +1,32 @@
 package main
 
-import ( 
+import (
+	"fmt"
 	"net/http"
 )
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("X-XSS-Protection", "1; mode=block") 
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
-		next.ServeHTTP(w, r) 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event // of a panic as Go unwinds the stack).
+		defer func() {
+			// Use the builtin recover function to check if there has been a // panic or not. If there has...
+			if err := recover(); err != nil {
+				// Set a "Connection: close" header on the response.
+				w.Header().Set("Connection", "close")
+				// Call the app.serverError helper method to return a 500 // Internal Server response.
+				app.serverError(w,
+					fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -19,6 +37,6 @@ func Auth(next http.Handler) http.Handler {
 			w.WriteHeader(http.StatusForbidden)
 			return
 		} */
-		next.ServeHTTP(w, r) 
+		next.ServeHTTP(w, r)
 	})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,40 +1,44 @@
 package main
 
-import ( 
+import (
 	"net/http"
-	//"github.com/justinas/alice"
+
+	"github.com/bmizerany/pat"
+
+	"github.com/justinas/alice"
 )
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	//Route Initialization
-	mux := http.NewServeMux()
-	//dynamicMiddleware := alice.New(app.session.Enable)
-	
+	mux := pat.New()
+	standardMiddleware := alice.New(app.recoverPanic, secureHeaders)
+	dynamicMiddleware := alice.New(app.session.Enable)
+
 	//All Required Routes
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux.Get("/", dynamicMiddleware.ThenFunc(app.home))
+	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
+	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
 	//Employee API's
 
 	//Got Issues with migration so created a api to create a table now
-	mux.HandleFunc("/employees/createTable", app.createEmpTable)
-	
+	//	mux.HandleFunc("/employees/createTable", app.createEmpTable)
+
 	//mux.HandleFunc("/employees/show", app.showAllEmpList)
-	mux.HandleFunc("/employees/create", app.createEmp)
-	mux.HandleFunc("/employees/updateEmp", app.updateEmployee)
+	//	mux.HandleFunc("/employees/create", app.createEmp)
+	//	mux.HandleFunc("/employees/updateEmp", app.updateEmployee)
 
 	// Add the five new routes.
-	/* mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm)) 
-	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser)) 
-	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm)) 
-	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser)) 
-	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser)) */
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
 
 	//Static Folder Path
-	fileServer := http.FileServer(http.Dir("./ui/static/")) 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	//fileServer := http.FileServer(http.Dir("./ui/static/"))
+	//	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	return mux
-	//return secureHeaders.Then(mux)
+	//return mux
+	return standardMiddleware(mux)
 }
